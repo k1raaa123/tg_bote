ce374e3384f0a3d0be9ea7c51edc84b5806c7245
k1raaa123 <zivilsk0573@mail.ru>
2022-04-19 14:19:07 +0300

    start

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ bot = telebot.TeleBot('5056881947:AAFwUCuBpIPUoovM0uok5-ptzwmy9kEMpKU')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == 'll':
+    if message.text.lower() == 'll':
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
 
-bot.polling(none_stop=True, interval=0)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
